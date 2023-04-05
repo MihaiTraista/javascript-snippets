abf57705ce640de56986804c4a2273c25d8eb5a5
MihaiTraista <mihai.musicmail@gmail.com>
2023-04-05 20:02:35 +0100

    new dir

---
+++ js-learn-1/sketch.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < 5; i++){
+  const child = document.createElement("div");
+  child.innerHTML = String(i);
+  child.class
+  child.onclick = function() {
+    alert("div #" + i);
+  }
+  box.appendChild(child);
+}
+
+const arr = [1, 2, 3];
+
+console.log(Array.isArray(arr), Object.prototype.toString.call(arr));
